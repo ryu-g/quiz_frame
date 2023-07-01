e2aa5558293f897264741c213884f03cf82fb20c
ryu-g <em.ryu.g@gmail.com>
2023-07-01 22:43:57 +0900

    add ANSWER-JUDGE

--- src/main.js
+++ src/main.js
@@ -15,16 +15,25 @@ const addJudgeEvent = ( list ) => {
         console.log(`ざんねん 正解は${quizData_correctChoice}`)
         reflesh_incorrectTimes()
       }
-      displayAnswers(button_list)
+      displayAnswers(button_list, i, quizData_correctChoice )
       view_quizDescription.classList = "description_display"
       utils.makeDisabelAllChoiceButton( button_list )
     })
     console.log(button_list)
   }
 }
-const displayAnswers = ( buttons ) => { //void
-  console.log(quizData_correctChoice)
+const displayAnswers = ( buttons, clicked, qd_correct ) => { //void
+  buttons[clicked].classList.add("clicked")
+  for(let i = 0; i < buttons.length; i++){
+    if(i == qd_correct){
+      buttons[i].classList.add("correct")
+    }
+    if(i != qd_correct){
+      buttons[i].classList.add("incorrect")
+    }
+  }
 }
+
 
 // const SRC_FILE = readFileSync("./quiz.json", 'utf8')
 const SRC_FILE = readFileSync("./src/quiz.json", 'utf8')
@@ -45,6 +54,7 @@ const choices_3 = document.getElementById("choice_3")
 const view_quizDescription = document.getElementById('quizDescription')
 const view_correnctTimes = document.getElementById('correnctTimes')
 const view_incorrenctTimes = document.getElementById('incorrenctTimes')
+const view_correnctRatio = document.getElementById('correctRatio')
 
 let choiceButtons = [choices_0, choices_1, choices_2, choices_3]
 let button_list = [choices_0, choices_1, choices_2, choices_3]
@@ -73,6 +83,10 @@ const reflesh_quiz = () =>{
   choices_1.innerText = quizData_choices[1] ?? "-"
   choices_2.innerText = quizData_choices[2] ?? "-"
   choices_3.innerText = quizData_choices[3] ?? "-"
+  choices_0.classList = ""
+  choices_1.classList = ""
+  choices_2.classList = ""
+  choices_3.classList = ""
   view_quizDescription.innerText = quizData_quizDescription
   choiceButtons = [choices_0, choices_1, choices_2, choices_3]
   utils.makeAbelAllChoiceButton(choiceButtons)
@@ -82,11 +96,17 @@ const reflesh_quiz = () =>{
 const reflesh_correctTimes = () => {
   view_correnctTimes.innerText = `正解数 : ${++quizData_correctTimes}`
   console.log("正解数を増やします")
+  reflesh_correctRatio(quizData_correctTimes, quizData_incorrectTimes)
 }
 
 let quizData_incorrectTimes = 0
 const reflesh_incorrectTimes = () => {
   view_incorrenctTimes.innerText = `不正解数 : ${++quizData_incorrectTimes}`
+  reflesh_correctRatio(quizData_correctTimes, quizData_incorrectTimes)
+}
+const reflesh_correctRatio = (c, i) =>{
+  let ratio =  (c / (c + i)) * 100
+  view_correnctRatio.innerText = `正解率 : ${ratio.toFixed(2)}%`
 }
 
 const promise = new Promise((resolve, reject) => {
